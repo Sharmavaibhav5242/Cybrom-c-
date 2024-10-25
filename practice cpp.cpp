b9cd496a68d0676ca4b505df03004e7001245406
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-10-25 21:21:53 +0530

    switch case weekdays

--- practice cpp.cpp
+++ practice cpp.cpp
@@ -1,13 +1,35 @@
-#include<iostream>;
+#include<iostream>
 using namespace std;
 int main()
-{ 
- int num;
- cout<<"Enter the digit"<<endl;
- cin>>num;
- for(int i=1 ; i<=10 ; i++)
- {
- 	cout<<num<<"*"<<i<<"="<<num*i<<endl;
- }
+{
+	int days;
+	cout<<"Enter number of weekdays (1-7)"<<endl;
+	cin>>days;
+	switch (days)
+	{
+		case 1:
+			cout<<"Sunday"<<endl;
+			break;
+				case 2:
+					cout<<"Monday"<<endl;
+					break;
+						case 3:
+							cout<<"Tuesday"<<endl;
+							break;
+								case 4:
+									cout<<"Wednesday"<<endl;
+									break;
+										case 5:
+											cout<<"Thursday"<<endl;
+											break;
+												case 6:
+													cout<<"Friday"<<endl;
+													break;
+													case 7:
+														cout<<"Saturday"<<endl;
+														break;
+															
+															default:
+																cout<<"invalid weekdays"<<endl;}
+	
 }
-
