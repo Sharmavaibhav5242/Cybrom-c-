733efba9caf5da46870872b6f52b5fbec7ed538c
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-10-28 23:32:48 +0530

    table using loops

--- concept of loops.cpp
+++ concept of loops.cpp
@@ -3,16 +3,8 @@ using namespace std;
 int main()
 {
 int num;
-int even=0;
-int odd=0;
-for(int i=1 ; i<=10 ;i++){ 
-cout<<"Enter a number"<<endl;
+cout<<"Enter a digit"<<endl;
 cin>>num;
-if(num%2==0)
-      even++;
-else
-       odd++;
-}
-	cout<<"Total number of even's are"<<"\t"<<even<<endl;
-	cout<<"Total number of odd's are"<<"\t"<<odd<<endl;
+for(int i=1;i<=10;i++)
+cout<<num*i<<endl;
 }
