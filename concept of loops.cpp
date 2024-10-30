eb61841ca6da5c8cec5f92b11c9fe9abc93defe4
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-10-30 23:20:02 +0530

    Area of the circle

--- concept of loops.cpp
+++ concept of loops.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-int num;
-cout<<"Enter a digit"<<endl;
-cin>>num;
-for(int i=1;i<=10;i++)
-cout<<num*i<<endl;
+const double pi=3.14;
+int Radius;
+cout<<"Enter radius of the circle"<<endl;
+cin>>Radius;
+cout<<"The radius of the cricle of given radius is " <<"="<<"\t"<<pi*Radius*Radius<<endl;
 }
