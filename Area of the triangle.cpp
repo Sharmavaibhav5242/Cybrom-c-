e87190df1b50ac8af86be2ca20d3c37f08f905c8
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-10-31 21:03:54 +0530

    Area of the triangle

---
+++ Area of the triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double base;
+	double height;
+	cout<<"Enter base and height of the triangle"<<endl;
+	cin>>base>>height;
+	const double d1=0.5;
+	cout<<"The area of the  triangle is"<<"="<<"\t"<<d1*base*height<<endl;
+}
