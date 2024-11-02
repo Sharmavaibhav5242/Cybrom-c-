36347067dc0c9c70ab57c189721c13a89a9058e7
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-11-02 23:45:03 +0530

    loops

--- Area of the triangle.cpp
+++ Area of the triangle.cpp
@@ -2,10 +2,30 @@
 using namespace std;
 int main()
 {
-	double base;
-	double height;
-	cout<<"Enter base and height of the triangle"<<endl;
-	cin>>base>>height;
-	const double d1=0.5;
-	cout<<"The area of the  triangle is"<<"="<<"\t"<<d1*base*height<<endl;
+//	double base;
+//	double height;
+//	cout<<"Enter base and height of the triangle"<<endl;
+//	cin>>base>>height;
+//	const double d1=0.5;
+//	cout<<"The area of the  triangle is"<<"="<<"\t"<<d1*base*height<<endl;
+int num;
+int even=0;
+int odd=0;
+for(int i=1;i<=10;i++){
+cout<<"Enter Number"<<endl;
+cin>>num;
+if (num%2==0)
+{
+	 even++ ;
+}
+else
+{
+	 odd++ ;
+}
+		
+}
+cout<<"Total number of even"<<"\t"<<even<<endl;
+cout<<"Total number of odd"<<"\t"<<odd<<endl;
+
+
 }
