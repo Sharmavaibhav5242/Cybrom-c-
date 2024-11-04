0d9cb64126fc1be678b142138e3b3e3743aa2b88
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-11-04 19:31:39 +0530

    pattern

--- Area of the triangle.cpp
+++ Area of the triangle.cpp
@@ -8,24 +8,38 @@ int main()
 //	cin>>base>>height;
 //	const double d1=0.5;
 //	cout<<"The area of the  triangle is"<<"="<<"\t"<<d1*base*height<<endl;
-int num;
-int even=0;
-int odd=0;
-for(int i=1;i<=10;i++){
-cout<<"Enter Number"<<endl;
-cin>>num;
-if (num%2==0)
+//int num;
+//int even=0;
+//int odd=0;
+//for(int i=1;i<=10;i++){
+//cout<<"Enter Number"<<endl;
+//cin>>num;
+//if (num%2==0)
+//{
+//	 even++ ;
+//}
+//else
+//{
+//	 odd++ ;
+//}
+//		
+//}
+//cout<<"Total number of even"<<"\t"<<even<<endl;
+//cout<<"Total number of odd"<<"\t"<<odd<<endl;
+for (int k=0;k<=3;k++)
 {
-	 even++ ;
-}
-else
+	for(int i=0;i<3;i++)
 {
-	 odd++ ;
+	cout<<i<<"=";
+	for (int j=i;j<3;j++)
+	{
+		cout<<" "<<"*";
+	}
+	cout<<endl;
 }
-		
+cout<<endl;
 }
-cout<<"Total number of even"<<"\t"<<even<<endl;
-cout<<"Total number of odd"<<"\t"<<odd<<endl;
+}
 
 
-}
+
