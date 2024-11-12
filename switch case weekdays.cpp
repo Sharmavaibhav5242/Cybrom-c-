39e30e3adc9f25ef42f1f40fdea0a1cdb7174812
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-11-12 12:50:16 +0530

    24 hour clock

--- switch case weekdays.cpp
+++ switch case weekdays.cpp
@@ -2,25 +2,60 @@
 using namespace std;
 int main()
 {
-int num1;
-int num2;
-char operation;
-cout<<"Enter numbers"<<endl;
-cin>>num1>>num2>>operation;
-switch(operation)
-{
-	case '+': cout<<num1+num2<<endl;
-	break;
-	case '-' : cout<<num1-num2<<endl;
-	break;
-	case '*' : cout<<num1*num2<<endl;
-	break;
-	case '/' : cout<<num1/num2<<endl;
-	break;
-	default : cout<<"invalid"<<endl;
-	break;
-}
- 
-
-
+ int time;
+ cout<<"Enter time"<<endl;
+ cin>>time;
+ switch(time)
+ {
+ 	case 12: cout<<"12 Noon";
+ 	break;
+ 	case 13: cout<<"1pm";
+ 	break;
+ 	case 14: cout<<"2pm";
+ 	break;
+ 	case 15: cout<<"3pm";
+ 	break;
+ 	case 16: cout<<"4pm";
+ 	break;
+ 	case 17: cout<<"5pm";
+ 	break;
+ 	case 18: cout<<"6pm";
+ 	break;
+ 	case 19: cout<<"7pm";
+ 	break;
+ 	case 20: cout<<"8pm";
+ 	break;
+ 	case 21: cout<<"9pm";
+ 	break;
+ 	case 22: cout<<"10pm";
+ 	break;
+ 	case 23: cout<<"11pm";
+ 	break;
+ 	case 0: cout<<"12 midnight";
+ 	break;
+ 	case 1: cout<<"1am";
+ 	break;
+ 	case 2:cout<<"2am";
+ 	break;
+ 	case 3:cout<<"3am";
+ 	break;
+ 	case 4:cout<<"4am";
+ 	break;
+ 	case 5:cout<<"5am";
+ 	break;
+ 	case 6:cout<<"6am";
+ 	break;
+ 	case 7:cout<<"7am";
+ 	break;
+ 	case 8:cout<<"8am";
+ 	break;
+ 	case 9:cout<<"9am";
+ 	break;
+ 	case 10:cout<<"10am";
+ 	break;
+ 	case 11:cout<<"11am";
+ 	break;
+ 	default:cout<<"Error"<<endl;
+ 	break;
+ }
 }
