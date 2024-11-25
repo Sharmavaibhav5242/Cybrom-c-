e2d1c1bcf8549c449f70a412e181bb61a5621898
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2024-11-25 18:40:12 +0530

    fibonaaci series

--- table.cpp
+++ table.cpp
@@ -1,24 +1,19 @@
+
 #include<iostream>
 using namespace std;
 int main ()
 {
-	int a=0;
-	for (int r=1;r<=5;++r)
-	{
-		(r<=5)?a++:a--;
-		for (int c=1;c<=9;++c)
-		{
-			if (c<=10-r && c>=r)
-			{
-				cout<<"*";
-			}
-			else
-			{
-				cout<<" ";
-			}
-		}
-		cout<<endl;
-	}
+int a=0;
+int b=1;
+int c;
+for (int i=1;i<=5;++i)
+{
+	c=a+b;
+	a=b;
+	b=c;
+	cout<<a;
+		
+	}	
 }
 
 
