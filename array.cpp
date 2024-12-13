b0d198229d0b80495abbf1c135f9f348e69de5b4
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-13 23:06:04 +0530

    Kth largest value in an array

--- array.cpp
+++ array.cpp
@@ -141,6 +141,129 @@ int main ()
 //        }
 //    }
 //    cout<<"Not Found";
+//int main ()
+//{
+//    int arr[]={1,2,1,3,4};
+//    int j=0;
+//     for(int i=0;i<5;++i)
+//     {
+//         if(arr[i]!=1)
+//         {
+//             arr[j]=arr[i];
+//             j++;
+//         }
+//     }
+//     for(int i=0;i<j;++i)
+//     {
+//         cout<<arr[i];
+//     }
+//   
+//}
+//{
+//    int arr[]={1,2,4,3,2};
+//    for (int i=0;i<5;++i)
+//    {
+//        for(int j=i+1;j<5;++j)
+//        {
+//            if(arr[i]==arr[j])
+//            arr[j]=-1;
+//        }
+//        
+//        
+//       
+//    }
+//    for(int i=0;i<5;++i)
+//        {
+//            if(arr[i]!=-1)
+//            {
+//                cout<<arr[i];
+//            }
+//        }
+//    
+//   
+//}{
+//    int arr[]={10,0,30,0,50};
+//    int j=0;
+//    for (int i=0;i<5;++i)
+//    {
+//        if(arr[i]!=0)
+//        {
+//            arr[j]=arr[i];
+//            j++;
+//        }
+//    }
+//    for(int i=j;i<5;++i)
+//    {
+//        arr[j]=0;
+//        j++;
+//    }
+//    for(int i=0;i<5;++i)
+//    {
+//        cout<<arr[i];
+//    }
+//}
+//#include <iostream>
+//using namespace std;
+//int main ()
+//{
+//int arr[]={10,12,13,14,15};
+//int j=0;
+//for(int i=0;i<4;++i)
+//{
+//    if(arr[i+1]-arr[i]>1)
+//    {
+//        cout<<arr[i]+1;
+//    }
+//    
+//
+//}
+//}
+//#include <iostream>
+//using namespace std;
+//int main ()
+//{
+//int arr[]={10,20,13,14,15};
+//int j=0;
+//for(int i=0;i<4;++i)
+//{
+//    if(arr[i+1]-arr[i]>1)
+//    {
+//     for(int j=arr[i]+1;j<arr[i+1];++j)
+//     {
+//         cout<<j<<endl;
+//     }
+//    }
+//    
+//
+//}
+//}
+//#include <iostream>
+//using namespace std;
+//int main ()
+//{
+//  int k;
+//  int j=0;
+//  int temp;
+//  cout<<"Enter the largest value to be found out";
+//  cin>>k;
+//  int arr[]={10,20,30,40,50};
+//  for(int i=0;i<5;++i)
+//  {
+//      for(int j=i+1;j<5;++j)
+//      {
+//          if(arr[j]>arr[i])
+//          {
+//            int temp;
+//            temp=arr[i];
+//            arr[i]=arr[j];
+//            arr[j]=temp;
+//          }
+//      }
+//}
+//
+//  cout<<arr[k-1];
+// 
+//}
 
 
 
