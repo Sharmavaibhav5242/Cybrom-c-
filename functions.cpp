dca7eeafb8bb3dfab9b20ed0a83555ce782094c4
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-16 23:26:47 +0530

    Start of functions

---
+++ functions.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//using namespace std;
+//int sum()
+//{
+//    int a=10;
+//    int b=20;
+//    return a+b;
+//}
+//int main()
+//{
+//    cout<<sum();
+//}
+//#include <iostream>
+//using namespace std;
+//void sum()
+//{
+//    cout<<"jhkhjhkh";
+//}
+//int main()
+//{
+//  sum();
+//}
+//#include <iostream>
+//using namespace std;
+//int sum(int a,int b)
+//{
+//    return a+b;
+//}
+//int main()
+//{
+//    cout<<sum(10,12);
+//}
+//#include <iostream>
+//using namespace std;
+//int sum(int a,int b)
+//{
+//    int c=a;
+//    int d=b;
+//    return c+d;
+//}
+//int main()
+//{
+//    int a;
+//    int b;
+//    cout<<"Enter two values";
+//    cin>>a>>b;
+//    cout<<sum(a,b);
+//}
