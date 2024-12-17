61a8c95736c49ea2f1532c0e7b5454abe7ef5201
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-17 20:47:59 +0530

    Call by pointer and Call by refrence in functions

--- functions.cpp
+++ functions.cpp
@@ -46,3 +46,39 @@
 //    cin>>a>>b;
 //    cout<<sum(a,b);
 //}
+//#include <iostream>
+//using namespace std;
+//// function= userdefined pre defined
+//// function with parameter without parameter
+//// function return type and non return type
+//int sum(int *a,int *b)
+//{
+//     return *a**b;
+//    //  pointer use *
+//      pointer dangling ho jata h isilye call by refrence use karenge
+//}
+//int main()
+//{ 
+//    int a,b;
+//    cout<<"Enter two number";
+//    cin>>a>>b;
+//    cout<<sum(&a,&b);
+//}
+//#include <iostream>
+//using namespace std;
+//// function= userdefined pre defined
+//// function with parameter without parameter
+//// function return type and non return type
+//int sum(int &b)
+//{
+//     return b;
+//    //  refrencing
+//    
+//}
+//int main()
+//{ 
+//    int a;
+//    cout<<"Enter number";
+//    cin>>a;
+//    cout<<sum(a);
+//}
