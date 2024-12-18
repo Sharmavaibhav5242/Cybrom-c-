a5a82400521ad070e5de4f1f8e6e496400dfae85
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-18 23:15:59 +0530

    call by value

--- functions.cpp
+++ functions.cpp
@@ -82,3 +82,28 @@
 //    cin>>a;
 //    cout<<sum(a);
 //}
+//#include <iostream>
+//using namespace std;
+//namespace first
+//{
+//    void display()
+//{
+//    cout<<"first";
+//    
+//}
+//}
+//namespace second
+//{
+//   void display()
+//{
+//    cout<<"second";
+//} 
+//}
+//
+//
+//int main()
+//{
+//    
+// first:: display();
+// second::display();
+//}
