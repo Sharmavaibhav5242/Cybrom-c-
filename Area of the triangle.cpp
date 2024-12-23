314b3ee0174dfd69b3e0c06668e0c3a5e514e06d
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-23 21:48:46 +0530

    Anagram checking in string

--- Area of the triangle.cpp
+++ Area of the triangle.cpp
@@ -1,78 +1,38 @@
 #include<iostream>
 using namespace std;
+bool anagram( string str1,string str2)
+{
+  int count[256]={0};
+  
+  for(int i=0;i<str1.length();++i)
+  {
+      count[str1[i]]++;
+      count[str2[i]]--;
+  }
+  for(int i=0;i<256;++i)
+  {
+      if(count[i]!=0)
+      {
+          return false;
+        
+         
+      }
+    
+  }
+    
+  return true;
+}
+
 int main()
 {
-//	double base;
-//	double height;
-//	cout<<"Enter base and height of the triangle"<<endl;
-//	cin>>base>>height;
-//	const double d1=0.5;
-//	cout<<"The area of the  triangle is"<<"="<<"\t"<<d1*base*height<<endl;
-//int num;
-//int even=0;
-//int odd=0;
-//for(int i=1;i<=10;i++){
-//cout<<"Enter Number"<<endl;
-//cin>>num;
-//if (num%2==0)
-//{
-//	 even++ ;
-//}
-//else
-//{
-//	 odd++ ;
-//}
-//		
-//}
-//cout<<"Total number of even"<<"\t"<<even<<endl;
-//cout<<"Total number of odd"<<"\t"<<odd<<endl;
-//int height;
-//cout<<"Enter the height of L shape"<<endl;
-//cin>>height;
-//for (int k=0;k<3;k++)
-//{
-//	cout<<endl;
-//for (int i=0;i<height-1;i++)
-//{
-//	cout<<"*"<<endl;
-//}
-//for (int i=0;i<height;i++)
-//{
-//	cout<<"* ";
-//}
-//cout<<endl;	
-//}
-//int a=0;
-//int b=1;
-//for (int i=1;i<=10;i++)
-//{
-//	a=a+b;
-//	b= a-b;
-//	if(a==21)
-//	continue;
-//	cout<<a<<endl;
-//}
-//cout<<"Process end"<<endl;
-
-int num1;
-int num2;
-char operation;
-cout<<"Enter numbers"<<endl;
-cin>>num1>>num2>>operation;
-switch(operation)
+string str1="vansh";
+string str2="anshve";
+if(anagram(str1,str2))
 {
-	case '+': cout<<num1+num2<<endl;
-	break;
-	case '-' : cout<<num1-num2<<endl;
-	break;
-	case '*' : cout<<num1*num2<<endl;
-	break;
-	case '/' : cout<<num1/num2<<endl;
-	break;
-	default : cout<<"invalid"<<endl;
-	break;
+    cout<<"anagram";
+}
+else
+{
+    cout<<"not";
 }
 }
-
-
-
--- functions.cpp
+++ functions.cpp
@@ -257,3 +257,39 @@
 //   }
 //    
 //}
+#include <iostream>
+using namespace std;
+bool anagram(string &str1,string &str2)
+{
+    char count[256]={0};
+    if(str1.length()!=str2.length())
+    {
+        return false;
+    }
+    for(int i=0;i<str1.length();++i)
+    {
+        count[str1[i]]++;
+        count[str2[i]]--;
+    }
+    for(int i=0;i<str1.length();++i)
+    {
+        if(count[i]!=0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    string str1="bittu";
+    string str2="titub";
+    if(anagram(str1,str2))
+    {
+        cout<<"Anagram";
+    }
+    else
+    {
+        cout<<"Not an anagram";
+    }
+}
