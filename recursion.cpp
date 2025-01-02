c1afe14138c53bfaf0ca425e7df120283e675a32
vaibhav <vaibhavsharma5242@gmail.com>
2025-01-03 01:41:41 +0530

    Fibonachi series using recursion

--- recursion.cpp
+++ recursion.cpp
@@ -12,17 +12,28 @@
 //{
 //    cout<<sum(4);
 //}
+//#include <iostream>
+//using namespace std;
+//int sum(int number)
+//{
+//    if(number<=1)
+//    {
+//        return number;
+//    }
+//    return number*sum(number-1);
+//}
+//int main()
+//{
+//    cout<<sum(4);
+//}
 #include <iostream>
 using namespace std;
-int sum(int number)
+int fibo(int number)
 {
-    if(number<=1)
-    {
-        return number;
-    }
-    return number*sum(number-1);
+	if(number<=1)return number;
+	return fibo(number-2)+fibo(number-1);
 }
 int main()
 {
-    cout<<sum(4);
+	cout<<fibo(10);
 }
