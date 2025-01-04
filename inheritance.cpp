65e8a5fb53d75f66c874775ef990c61726e9e62d
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-01-04 19:12:08 +0530

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//class a
+//{
+//    public:int roll=007;
+//};
+//class b
+//{
+//   public:string name="ashu";
+//   
+//};
+//class c:public b , public a
+//{
+//   public: void display()
+//    {
+//        cout<<roll<<name;
+//    }
+//};
+//int main()
+//{
+//   c obj;
+//   obj.display();
+//}
