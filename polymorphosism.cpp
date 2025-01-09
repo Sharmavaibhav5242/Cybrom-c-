9530a6f17df191ffe97e327ca167288b3a573c6f
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-01-09 23:22:18 +0530

    polymorphosism

---
+++ polymorphosism.cpp
@@ -0,0 +1,86 @@
+//#include <iostream>
+//using namespace std;
+//void sum()
+//{
+//    cout<<"Animal";
+//}
+//void sum(int a)
+//{
+//    cout<<a;
+//}
+//int main()
+//{
+//    sum();
+//    sum(10);
+//}
+//#include <iostream>
+//using namespace std;
+//class A
+//{
+//    int value;
+//    public:A(int x)
+//    {
+//        int value=x;
+//        cout<<x;
+//    }
+//};
+//int main()
+//{
+//    A obj(10);
+//}
+//#include <iostream>
+//using namespace std;
+//class A
+//{ 
+//    public:
+//    int value;
+//    public:A(int x=0)
+//    {
+//     value=x;
+//    }
+//   A add(A obj)
+//    {
+//         A obj2;
+//        obj2.value=value+obj.value;
+//        return obj2;
+//        
+//    }
+//    public:void show()
+//    {
+//        cout<<value;
+//    }
+//};
+//int main()
+//{
+//    A obj(10);
+//    A obj1(11);
+//    A obj3;
+//    obj3=obj.add(obj1);
+//    obj3.show();
+//}
+//#include <iostream>
+//using namespace std;
+//class A 
+//{
+//    public:
+//   virtual void show()
+//    {
+//        cout<<"Class a";
+//    }
+//};
+//class B: public A
+//{
+//    public:
+//    void show()
+//    {
+//        cout<<"Class b";
+//    }
+//};
+//int main()
+//{
+//    A *obj;
+//    B obj1;
+//    obj=&obj1;
+//    obj->show();
+//    
+//}
